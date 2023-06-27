05014c72aa9c8d326836305bb146f0d1c11a8e9b
kulaizki <kulaizke@gmail.com>
2023-06-28 00:54:59 +0800

    deploy

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -24,25 +24,25 @@ function Skills() {
       </div>
       <div className="Skills__cards">
         <InfoCard
-          img="/encrusted-lock.png"
+          img="./encrusted-lock.png"
           title="Competitive Programming"
           description="I have a passion for problem-solving, which led me to pursue competitive programming as a hobby. I'm proud to share that I recently achieved a significant milestone by qualifying for the UP-ACM Algolympics 2023 Finals."
           link="https://github.com/kulaizki/Codeforces"
         />
         <InfoCard
-          img="/landing-page.png"
+          img="./landing-page.png"
           title="Web Development"
           description="I have extensive experience in front-end web development, specializing in creating engaging user interfaces. With a keen eye for design, I take pleasure in crafting visually appealing experiences for users."
           link="https://github.com/kulaizki?tab=repositories"
         />
         <InfoCard
-          img="/harmonic-chart.png"
+          img="./harmonic-chart.png"
           title="Trading / Investments"
           description="As an enthusiastic and determined learner, I dedicate myself to studying and actively practicing trading and investments, nurturing my profound interest in the dynamic and ever-evolving finance industry."
           link="https://www.tradingview.com/u/kulaizkii/"
         />
         <InfoCard
-          img="/c-programming.png"
+          img="./c-programming.png"
           title="C programming"
           description="Given the widespread prevalence of the C programming language at my university, I naturally developed a keen interest in it and have gained substantial expertise through extensive hands-on experience."
           link="https://github.com/kulaizki/BSCS1"
