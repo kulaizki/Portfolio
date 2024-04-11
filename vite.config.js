5df0c79c7457985a5bab72dca368630437962975
kulaizki <kulaizke@gmail.com>
2024-04-11 17:54:56 +0800

    fix: whitepage error

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://kulaizki.github.io/Portfolio/',
+  base: '/Portfolio',
   plugins: [react()],
 })
