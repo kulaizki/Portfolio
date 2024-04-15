563b0fc54f74f9c1b420316e1dbfd628f7207a08
kulaizki <kulaizke@gmail.com>
2024-04-15 16:10:43 +0800

    feat: add base footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/layout/Header";
 import Intro from "./components/intro/Intro";
 import Skills from "./components/skills/Skills";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <hr />
         <Skills />
       </main>
+      <Footer />
     </div>
   );
 }
