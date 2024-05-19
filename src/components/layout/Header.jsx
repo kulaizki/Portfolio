f5f78800cfe7d4610dc15341a5c5caccfad82297
kulaizki <kulaizke@gmail.com>
2024-05-19 22:15:19 +0800

    fix: header img

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="Header">
       <h1 className="Header__logo">
-        <img src="./public/yellow-diamond.png" alt="" />
+        <img src="./yellow-diamond.png" alt="" />
         <a href="#">Portfolio</a>
       </h1>
       <div className="Header__copyright">Fitz Angelo&nbsp;2024&nbsp;&copy;</div>
